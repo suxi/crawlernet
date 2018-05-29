e135fb846d2bbd258cb26769e3605d43f11d21ec
suxi <suxi@xiaohulu.com>
2018-05-29 17:02:05 +0800

    add image picking

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -16,13 +18,9 @@ namespace crawlernet
         {
 
             var key = args[0].ToString();
-            var reg = new Regex($"<a[^<]*href=\"([^<\"]*)\"[^<]*>([^<]*{key}[^<]*)",
-            RegexOptions.IgnoreCase|
-            RegexOptions.Multiline|
-            RegexOptions.Compiled|
-            RegexOptions.IgnorePatternWhitespace);
+            
 
-            var client = new HttpClient();
+            var client = new HttpClient(){Timeout=new TimeSpan(0,3,0)};
             var links = new ConcurrentBag<string>();
             var index = Enumerable.Range(1, 700).ToArray();
             var part = Partitioner.Create<int>(index);
@@ -32,35 +30,74 @@ namespace crawlernet
                 {
                     return await client.GetStringAsync(uri);
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
-                    Console.WriteLine($"err: {e.Message} {uri}");
                     return "";
                 }
                 
-            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 20 });
+            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 8 });
 
-            var grep = new ActionBlock<string>(text => 
+            var grep = new TransformManyBlock<string,string>(async text => 
             {
+                var l = new List<string>();
+                var reg = new Regex($"<a[^<]*href=\"([^<\"]*)\"[^<]*>([^<]*{key}[^<]*)",
+                    RegexOptions.IgnoreCase|
+                    RegexOptions.Multiline|
+                    RegexOptions.Compiled|
+                    RegexOptions.IgnorePatternWhitespace);
                 var matches = reg.Matches(text);
                 foreach (Match match in matches)
                 {
                     if (!links.Contains(match.Groups[1].ToString()))
                     {
-                        Console.WriteLine($"http://n2.lufi99.org/pw/{match.Groups[1]} {match.Groups[2]}");
                         links.Add(match.Groups[1].ToString());
+                        var res = await client.GetStringAsync($"http://n2.lufi99.org/pw/{match.Groups[1]}");
+                        var reg2 = new Regex("width>=1024[^(]*\\('([^']*)[^>]*>");
+                        var match2 = reg2.Match(res);
+                        var picker = "";
+                        if (match2.Success)
+                        {
+                            picker = match2.Groups[1].Value;
+                            l.Add(picker);
+                        }
+                        Console.WriteLine($"http://n2.lufi99.org/pw/{match.Groups[1]} {picker} {match.Groups[2]}");
+
                     }
 
                 }
-            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 100 });
+                return l;
+            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 8 });
 
+            var save = new ActionBlock<string>(async url => {
+                var uri = new Uri(url);
+                try
+                {
+                    var res = await client.GetAsync(uri);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        using (var file = new FileStream($"{Directory.GetCurrentDirectory()}\\img\\{uri.Segments.Last()}", FileMode.OpenOrCreate))
+                        {
+                            using (var s = await res.Content.ReadAsStreamAsync())
+                            {
+                                await s.CopyToAsync(file);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 4 });
             download.LinkTo(grep, new DataflowLinkOptions { PropagateCompletion = true });
+            grep.LinkTo(save, new DataflowLinkOptions { PropagateCompletion = true });
             Parallel.ForEach(part, page => 
             {
                 download.Post($"http://n2.lufi99.org/pw/thread.php?fid=22&page={page}");
             });
             download.Complete();
-            grep.Completion.Wait();
+            save.Completion.Wait();
+            Console.WriteLine("搜索完成");
             return;
         }
     }
